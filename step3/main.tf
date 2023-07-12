832b53306f710ecb06f618b2638fc15c5efa2b87
Navneet Verma <47264956+papivot@users.noreply.github.com>
2023-07-12 16:12:58 -0400

    mgmt

--- step3/main.tf
+++ step3/main.tf
@@ -92,23 +92,12 @@ resource "avi_ipamdnsproviderprofile" "wcp_dns" {
 resource "avi_cloud" "vmware_cloud_wcp" {
   name                              = var.cloud_name
   vtype                             = "CLOUD_VCENTER"
-#  autoscale_polling_interval        = 60
-#  metrics_polling_interval          = 60
-#  mtu                               = 1500
-#  vmc_deployment                    = false
-#  dns_resolution_on_se              = false
-#  enable_vip_on_all_interfaces      = false
-#  enable_vip_static_routes          = false
-#  ip6_autocfg_enabled               = false
-#  maintenance_mode                  = false
-#  prefer_static_routes              = false
-#  state_based_dns_registration      = true
   dhcp_enabled                      = true
   license_tier                      = "ENTERPRISE"
   license_type                      = "LIC_CORES"
   ipam_provider_ref                 = avi_ipamdnsproviderprofile.wcp_ipam.id
   dns_provider_ref                  = avi_ipamdnsproviderprofile.wcp_dns.id
-  se_group_template_ref             = "https://${var.avi_controller_ips[0]}/api/serviceenginegroup/${data.avi_serviceenginegroup.wcp_serviceenginegroup.uuid}"
+# se_group_template_ref             = "https://${var.avi_controller_ips[0]}/api/serviceenginegroup/${data.avi_serviceenginegroup.wcp_serviceenginegroup.uuid}"
   tenant_ref                        = data.avi_tenant.tenant.id
   vcenter_configuration {
     privilege               = "WRITE_ACCESS"
@@ -117,19 +106,12 @@ resource "avi_cloud" "vmware_cloud_wcp" {
     vcenter_url             = var.vcenter_url
     datacenter              = var.vcenter_datacenter
     use_content_lib         = false
-    deactivate_vm_discovery = false
-#    is_nsx_environment      = false
     content_lib {
       id = data.vsphere_content_library.library.id
     }
-    management_network      = "https://${var.avi_controller_ips[0]}/api/vimgrnwruntime/${data.vsphere_network.wcp_mgmt_network.id}-${data.avi_cloud.default.uuid}"
-#    management_network      = var.vcenter_management_network
+    management_network      = var.vcenter_management_network
+#   management_network      = "https://${var.avi_controller_ips[0]}/api/vimgrnwruntime/${data.vsphere_network.wcp_mgmt_network.id}-${data.avi_cloud.default.uuid}"
   }
-#  lifecycle {
-#    ignore_changes = [
-#	      vcenter_configuration
-#    ]
-#  }
   depends_on = [
     avi_ipamdnsproviderprofile.wcp_dns, avi_ipamdnsproviderprofile.wcp_ipam, avi_vrfcontext.vrf_global
   ]
